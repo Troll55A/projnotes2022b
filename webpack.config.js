f4a79e5fbd67c70bde1e2991fabcbc1f5723b3c4
Morales Guz <luis_muscul30@hotmail.com>
2022-09-26 08:14:09 -0500

    agregamos webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+//El archivo de configuracion debe usar ES5
+
+//Importar un administrador de rutas de archivos
+const path = require('path');
+
+//Exportamos un objeto de configuracion, que sera usado por webpack
+module.exports ={}
