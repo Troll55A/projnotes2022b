327ef2c60ba4248c337e1798fdef31b0e1c4ddff
Morales Guz <luis_muscul30@hotmail.com>
2022-09-26 08:36:14 -0500

    lo mismo

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,25 @@
 const path = require('path');
 
 //Exportamos un objeto de configuracion, que sera usado por webpack
-module.exports ={}
+module.exports ={
+//1. El archivo de entrada o indexador
+    entry: "./client/index.js",
+//2. Especificar el archivo de salida
+output:{
+path:path.resolve(__dirname,"public"),
+//2.2 Nombre del archivo de salida
+filename:"bundle.js"
+},
+//3.Configuranco el servidor de desarrollo
+devServer:{
+    //3.1 Folder de archivos estaticos
+    static:path.join(__dirname,"public"),
+    //3.2 Puerto del servidor de desarrollo
+    port:8080,
+    //3.3 Definiendo el host
+    host:"localhost"
+}
+}
+
+
+
