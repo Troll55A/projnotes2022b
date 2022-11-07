06b5d97cb0d303935f8d478fd4c7d243b2903863
Morales Guz <luis_muscul30@hotmail.com>
2022-11-07 08:16:59 -0600

    Integrando materialize

--- client/index.js
+++ client/index.js
@@ -2,4 +2,10 @@
 import './stylesheet/myStyles.css';
 import './stylesheet/style.css';
 
+// Cargado estilos de mzterialize css
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de materialize
+import 'materialize-css/dist/js/materialize';
+
+M.AutoInit();
 console.log('Webpack Working Middleware!!! 🍞');
