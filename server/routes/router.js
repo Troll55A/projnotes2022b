0ab40a1cb140af6105919aed73273142f323d8cd
Morales Guz <luis_muscul30@hotmail.com>
2022-11-28 19:10:46 -0600

    Handout

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,11 @@
 // Importando los tramos de rutas
 import homeRouter from '../domains/home/homeRouter';
+import projectRouter from '../domains/project/projectRouter';
 
 // Funcion de enrutado principal
 const addRoutes = (app) => {
   // Agregando enrutado de Home
   app.use('/', homeRouter);
+  app.use('/project', projectRouter);
 };
-
 export default { addRoutes };
