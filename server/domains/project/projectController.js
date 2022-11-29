d1c60b62548037c21b8790ba3c3ec47600af105f
Morales Guz <luis_muscul30@hotmail.com>
2022-11-28 19:52:23 -0600

    avnce 1 de Validación

--- server/domains/project/projectController.js
+++ server/domains/project/projectController.js
@@ -12,9 +12,26 @@ const list = (req, res) => {
 
 // GET "/project/add"
 // GET "/project/create"
-const add = (req, res) => {
+const showAddProjectForm = (req, res) => {
   const viewModel = {};
   res.render('project/add', viewModel);
 };
+
+// POST "/project/add"
+// POST "/project/create"
+const addProject = (req, res) => {
+  // Desesctructurando y renombrando error de datos
+  const { errorData: error } = req;
+  // Verificando si hay error de validación
+  if (error) {
+    res.status(200).json(error);
+  } else {
+    // Desestructurando datos del formulario
+    const { validData: projectData } = req;
+    // Contestando los datos del proyecti
+    res.status(200).json(projectData);
+  }
+};
+
 // Exportando el Controlador
-export default { list, add };
+export default { list, showAddProjectForm, addProject };
--- server/domains/project/projectRouter.js
+++ server/domains/project/projectRouter.js
@@ -2,6 +2,10 @@
 import { Router } from 'express';
 // Importando el controlador
 import projectController from './projectController';
+// Importando factoria de validación
+import ValidateFactory from '../../services/validateFactory';
+// Importando objeto validador
+import projectValidator from './projectValidator';
 // Creando una isntancia del enrutador
 const router = new Router();
 
@@ -12,7 +16,17 @@ router.get(['/', '/list'], projectController.list);
 
 // Enrutamos
 // GET "/project/add"
-router.get(['/add', '/create'], projectController.add);
+router.get(['/add', '/create'], projectController.showAddProjectForm);
+
+// POST "/project/add"
+router.post(
+  ['/add', '/create'],
+  ValidateFactory({
+    schema: projectValidator.projectSchema,
+    getObject: projectValidator.getProject,
+  }),
+  projectController.addProject
+);
 
 // Exporto este tramo de ruta
 export default router;
